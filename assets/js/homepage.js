ceb8c21fe6462f9396a7432bbf428372a680e734
MJerich <MatthewJerich@gmail.com>
2022-03-08 15:23:33 -0700

    js setup

---
+++ assets/js/homepage.js
@@ -0,0 +1,12 @@
+function getUserRepos(user) {
+    // format the github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+getUserRepos("MJerich");
